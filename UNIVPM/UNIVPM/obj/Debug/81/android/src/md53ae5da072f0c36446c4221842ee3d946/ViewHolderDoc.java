5456f4af7c766e4a535d7b618e91cc47660e50a9
giubbilo <92432998+giubbilo@users.noreply.github.com>
2023-03-04 00:57:14 +0100

    Upload files

---
+++ UNIVPM/UNIVPM/obj/Debug/81/android/src/md53ae5da072f0c36446c4221842ee3d946/ViewHolderDoc.java
@@ -0,0 +1,38 @@
+package md53ae5da072f0c36446c4221842ee3d946;
+
+
+public class ViewHolderDoc
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("UNIVPM.Resources.ViewHolderDoc, UNIVPM", ViewHolderDoc.class, __md_methods);
+	}
+
+
+	public ViewHolderDoc ()
+	{
+		super ();
+		if (getClass () == ViewHolderDoc.class)
+			mono.android.TypeManager.Activate ("UNIVPM.Resources.ViewHolderDoc, UNIVPM", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
